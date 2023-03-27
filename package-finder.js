005f987874ddb263007bf63a95accbff806b9a75
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-03-27 15:31:43 -0600

    updating package-finder.js to create readme from fetched packages; moved original README.md into /docs .

--- package-finder.js
+++ package-finder.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
+
+
+
 module.exports = async ({github}) => {
- return github.rest.packages.listPackagesForOrganization({
+ const pkgs = await github.rest.packages.listPackagesForOrganization({
     package_type: 'container',
     org: 'defenseunicorns'
   });
+
+ makeReadme(pkgs)
+}
+
+function makeReadme(pkgs) {
+
+  let readme_table = '| Package | URL |\n' +
+                     '|---------|-----|\n';
+
+  for(const pkg of pkgs.data) {
+    readme_table += `${pkg.name} | [${pkg.url}](${pkg.url}) |\n`
+  }
+
+  fs.writeFileSync('README.md', readme_table, 'utf-8');
 }

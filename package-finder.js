5f9edbed38f54244d4dbc1bf59ff9321b3aaf2b7
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-03-27 08:42:39 -0600

    removed unneeded args in package find workflow script; stringified package_type in package-finder.js.

--- package-finder.js
+++ package-finder.js
@@ -1,6 +1,6 @@
 module.exports = async ({github}) => {
  return github.rest.packages.listPackagesForOrganization({
-    package_type: container,
+    package_type: 'container',
     org: 'defenseunicorns'
   });
 }

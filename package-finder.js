0d0ce6cc8e8f3700fadaf1bda6f563f03198bd9d
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-03-29 08:12:19 -0600

    testing getting tags for latest version of package and adding tags to readme in workflow.

--- package-finder.js
+++ package-finder.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-
-
 module.exports = async ({github}) => {
  const pkgs = await github.rest.packages.listPackagesForOrganization({
     package_type: 'container',
@@ -11,13 +9,23 @@ module.exports = async ({github}) => {
  makeReadme(pkgs)
 }
 
-function makeReadme(pkgs) {
 
-  let readme_table = '| Package | Repo |\n' +
-                     '|---------|-----|\n';
+async function getPkgVersions(pkgName) {
+  return github.rest.packages.getAllPackageVersionsForPackageOwnedByOrg({
+		package_type: 'container',
+		package_name: pkgName,
+		org: 'defenseunicorns'
+	});
+}
+
+async function makeReadme(pkgs) {
+
+  let readme_table = '| Package | Repo | Tags |\n' +
+                     '|---------|------|------|\n';
 
   for(const pkg of pkgs.data) {
-    readme_table += `[${pkg.name}](${pkg.html_url}) | ${pkg.repository && pkg.repository.name} |\n`
+    const versions = await getPkgVersions(pkg.name)
+    readme_table += `[${pkg.name}](${pkg.html_url}) | [${pkg.repository && pkg.repository.name}](${pkg.repository.html_url}) | ${versions[0].metadata.container.tags.join('<br />')} |\n`
   }
 
   fs.writeFileSync('README.md', readme_table, 'utf-8');

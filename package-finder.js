fe047cb0c4b07a21bb496b67cb0f1055294b5b0d
TristanHoladay <40547442+TristanHoladay@users.noreply.github.com>
2023-03-29 11:09:50 -0600

    refactored conditional checks into easier to read variables.

--- package-finder.js
+++ package-finder.js
@@ -31,7 +31,11 @@ async function makeReadme() {
 
   for(const pkg of pkgs.data) {
     const versions = await getPkgVersions(pkg.name)
-    readme_table += `[${pkg.name}](${pkg.html_url}) | [${pkg.repository && pkg.repository.name}](${pkg.repository.html_url}) | ${versions[0] && versions[0].metadata.container.tags.join('<br />')} |\n`
+    const repo_name = pkg.repository && pkg.repository.name || '';
+    const repo_url = pkg.repository && pkg.repository.html_url || '';
+    const latest_version_tags = versions[0] && versions[0].metadata.container.tags.join('<br />')
+    
+    readme_table += `[${pkg.name}](${pkg.html_url}) | [${repo_name}](${repo_url}) | ${latest_version_tags} |\n`
   }
 
   fs.writeFileSync('README.md', readme_table, 'utf-8');
